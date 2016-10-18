60693d6ea802c26886926b3e2b3737043e177a94
Martin Paoloni <martinpaoloni@gmail.com>
2016-10-18 09:43:36 -0300

    Fix iteration on XSNamedMap

--- wadl-raml-codegen-core/src/main/java/org/wadl/model/builder/ApplicationBuilder.java
+++ wadl-raml-codegen-core/src/main/java/org/wadl/model/builder/ApplicationBuilder.java
@@ -13,33 +13,34 @@ import org.apache.xerces.xs.XSImplementation;
 import org.apache.xerces.xs.XSLoader;
 import org.apache.xerces.xs.XSModel;
 import org.apache.xerces.xs.XSNamedMap;
+import org.apache.xerces.xs.XSObject;
 import org.mulesoft.web.app.model.ApplicationModel;
 import org.mulesoft.web.app.model.ResourceModel;
-import org.mulesoft.web.app.model.ResourceTypeModel;
 import org.w3c.dom.Element;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.ls.LSInput;
 
 
 public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
-    
+
 	public ApplicationBuilder(Class<ApplicationModel> modelClass) {
 		super(modelClass);
 	}
 
 	private final String DEFAULT_API_TITLE = "Enter API title here";
-    
+
+    @Override
     public void fillModel(ApplicationModel app, Element element) throws Exception{
-    	
+
         extractDocumentation(element, app);
-        
+
         Map <String, String> includedSchemas = getSchemas(element, pathResolver);
         app.setIncludedSchemas(includedSchemas);
-        
+
         ResourceBuilder resourceBuilder = getBuildManager().getBuilder(ResourceBuilder.class);
         List<Element> resourcesElements = Utils.extractElements(element, "resources");
         for(Element resourcesElement : resourcesElements){
-            
+
             String baseUri = resourcesElement.getAttribute("base");
             String title = resourcesElement.getAttribute("title");
             if(app.getBaseUri()==null){
@@ -47,11 +48,11 @@ public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
             }
             if (app.getTitle() == null && !title.equals("")){
             	app.setTitle(title);
-            } 
+            }
             else {
             	app.setTitle(DEFAULT_API_TITLE);
             }
-            
+
             List<Element> resourceElements = Utils.extractElements(resourcesElement,"resource");
             for(Element resourceElement : resourceElements){
 				ResourceModel res = resourceBuilder.build(resourceElement);
@@ -59,7 +60,7 @@ public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
                 app.addResource(res);
             }
         }
-        
+
         ResourceTypeBuilder resourceTypeBuilder = getBuildManager().getBuilder(ResourceTypeBuilder.class);
         List<Element> resourceTypeElements = Utils.extractElements(element, "resource_type");
         for(Element resourceTypeElement : resourceTypeElements){
@@ -71,34 +72,35 @@ public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
         for(Element methodElement: methodElements){
         	methodBuilder.build(methodElement);
         }
-        
+
         RepresentationBuilder representationBuilder = getBuildManager().getBuilder(RepresentationBuilder.class);
         List<Element> representationElements = Utils.extractElements(element, "representation");
         for(Element representationElement: representationElements){
         	representationBuilder.build(representationElement);
         }
     }
-    
+
     private Map<String, String> getSchemas(Element element, IPathResolver pathResolver) throws IOException {
-    	
+
     	List<String> includePaths = getIncludePaths(element);
-    	
+
     	Map<String,String> schemas = new HashMap<String, String>();
-    	
+
     	for (String path : includePaths){
 
     		String schemaContent = pathResolver.getContent(path);
     		List<String> elementNames = getRootElementNames(schemaContent);
-    		
+
     		for(String str : elementNames){
     			schemas.put(str, schemaContent);
     		}
     	}
     	return schemas;
 	}
-    
-	private List<String> getRootElementNames(String content) {
-		
+
+	@SuppressWarnings("unchecked")
+    private List<String> getRootElementNames(String content) {
+
 		ArrayList<String> list = new ArrayList<String>();
 		String propertyValue = System.getProperty(DOMImplementationRegistry.PROPERTY);
 
@@ -116,10 +118,12 @@ public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
 			XSModel model = schemaLoader.load(input);
 
 			XSNamedMap declarations = model.getComponents(XSConstants.ELEMENT_DECLARATION);
-			
-			for(int i = 0 ; i < declarations.size() ; i++){
-				String schemaName = declarations.item(i).getName();
-				list.add(schemaName);
+
+			int i = 0;
+			XSObject object = declarations.item(i);
+			while (object != null) {
+			    list.add(object.getName());
+				object = declarations.item(++i);
 			}
 
 		} catch (Exception e) {
@@ -139,11 +143,12 @@ public class ApplicationBuilder extends AbstractBuilder<ApplicationModel> {
 	private List<String> getIncludePaths (Element element){
     	List<String> includePaths = new ArrayList<String>();
     	List<Element> grammars = Utils.extractElements(element, "grammars");
-    	 
+
     	for (Element grammar : grammars){
-    		List<Element> includes = Utils.extractElements(grammar, "include");  
-    		for (Element include : includes)
-    			includePaths.add(include.getAttribute("href"));
+    		List<Element> includes = Utils.extractElements(grammar, "include");
+    		for (Element include : includes) {
+                includePaths.add(include.getAttribute("href"));
+            }
     	}
     	return includePaths;
     }
